170ec26298ce7f6a201779b602a0d675435205ea
d1n2o3b4 <davebostons2017@gmail.com>
2026-02-15 22:23:45 +0100

    added serializers

--- IMDB/urls.py
+++ IMDB/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('watchlist.urls'))
+    path('movie/', include('watchlist.api.urls')),
 ]
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render
-from watchlist.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from .models import Movie
+# from django.http import JsonResponse
+# from django.shortcuts import get_object_or_404
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies': list(movies.values())
-    }
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies': list(movies.values())
+#     }
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
-def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        "name": movie.title,
-        "description": movie.description,
-        "active": movie.active
-    }
+# def movie_details(request, pk):
+#     movie = get_object_or_404(Movie, pk=pk)
+#     data = {
+#         "title": movie.title,
+#         "description": movie.description,
+#         "active": movie.active
+#     }
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
     
